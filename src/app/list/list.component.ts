82c6c79cd27fc3a454f84c8e16cb8256eae05e67
Grimto <safouene.chahed@medtech.tn>
2019-09-29 20:55:50 +0200

    x

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Item} from '../../assets/model/item';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -8,19 +8,43 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 })
 export class ListComponent implements OnInit {
   items;
-
+  ar;
   constructor(private httpService: HttpClient) {
   }
 
   ngOnInit(): void {
-    this.httpService.get(``).subscribe(
+    this.httpService.get(`http://localhost:8082/fridge/getContent?fridgeId=0`).subscribe(
       data => {
         this.items = data as string [];	 // FILL THE ARRAY WITH DATA.
+        const listOfUPC = [];
+        for (const item of this.items) {
+          listOfUPC.push(item.upc14);
+        }
+
+        let params = new HttpParams();
+
+        params = params.append('upcs', listOfUPC.join(','));
+        this.httpService.get(`http://localhost:8081/grocery/identify` , {params : params}).subscribe(
+          data2 => {
+            this.ar = data2 as string[];
+            for (const item of this.ar) {
+              for (const t of this.items) {
+                if (item.upc14 === t.upc14) {
+                  t.name = item.name;
+                }
+              }
+            }
+          },
+          (err: HttpErrorResponse) => {
+            console.log(err.message);
+          }
+        );
       },
       (err: HttpErrorResponse) => {
         console.log(err.message);
       }
     );
+
   }
 
 }
